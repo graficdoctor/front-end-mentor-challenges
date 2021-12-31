605cc98c09cd83b0ab36e23e340043d7842f70f2
Katrien Schuermans <katrien@MacBook-Pro.lan>
2021-12-31 12:20:38 +0100

    deleted 12

--- 12-article-preview-component/app.js
+++
@@ -1,8 +0,0 @@
-const shareButtonElement = document.getElementById('share-btn');
-const shareSocialMediaElement = document.getElementById('share-social-media');
-
-function shareSocialMedia() {
-  shareSocialMediaElement.classList.toggle('active');
-}
-
-shareButtonElement.addEventListener('click', shareSocialMedia);
